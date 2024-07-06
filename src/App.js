579b8e05b26407ffd37159576d28c48ee544b75a
sri582 <srikrishnar007@gmail.com>
2024-07-06 19:00:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Header from './container/Header/Header';
+import Intro from './container/Intro/Intro';
+import Laurels from './container/Laurels/Laurels';
+import Gallery from './container/Gallery/Gallery';
+import FindUs from './container/Findus/FindUs';
+import Footer from './container/Footer/Footer';
+import AboutUs from './container/AboutUs/AboutUs';
+import SpecialMenu from './container/Menu/SpecialMenu';
+import MenuPage from './container/Menu/MenuPage';
 import './App.css';
+import './index.css';
+import MenuItem from './components/Menuitem/MenuItem';
+import Chef from './container/Chef/Chef';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Header />
+              <AboutUs />
+              <SpecialMenu />
+              <MenuItem />
+              <Chef />
+              <Intro />
+              <Laurels />
+              <Gallery />
+              <FindUs />
+              <Footer />
+            </>
+          } />
+          <Route path="/menu-page" element={<MenuPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
+
