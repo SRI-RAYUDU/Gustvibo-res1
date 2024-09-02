f6d73a28f1f13c0ff08eb42290317de29ecaa95c
sri582 <srikrishnar007@gmail.com>
2024-09-02 12:43:16 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -39,7 +39,6 @@ const App = () => {
                 <Gallery />
                 <FindUs />
                 <Footer />
-                <SubscriptionThankYou/>
               </>
             }
           />
